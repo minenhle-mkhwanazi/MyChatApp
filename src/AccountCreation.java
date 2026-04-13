0dd7e8707ed0be6c28fc4f61c75ec9a7a43f8628
Student <Student@LabVM2047644.edxzm0bkzfgeza4wb0ahgpg3ha.jnbx.internal.cloudapp.net>
2026-04-13 18:25:18 +0200

    My first commit

--- src/AccountCreation.java
+++ src/AccountCreation.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-public class AccountCreation {
+public class AccountCreation { 
     
      Scanner input = new Scanner(System.in);
      
